1819f1671b0e321a34c4d187cf38adab3fe6f3d0
28kayak <kaya_ota@msn.com>
2013-05-06 14:34:04 -0700

    add method named replace

--- Tree.java
+++ Tree.java
@@ -3,5 +3,27 @@ public class Tree
 {
 	protected TreeNode root = null;
 	
+	protected void replace(TreeNode parent, TreeNode newNode, TreeNode oldNode)
+	{
+		if(parent == null)
+		{
+			this.root = newNode;
+			return;
+		}
+		if(parent.left == oldNode)
+		{
+			parent.left = newNode;
+		}
+		else if(parent.right == oldNode)
+		{
+			parent.right = newNode;
+		}
+		else
+		{
+			throw new RuntimeException("No children!!!");
+			//this Exception is thrown when parent is NOT null
+			//and parent does not have any child.
+		}
+	}
 
 }
