13f76a4d5fc409cf0c39a4a0731d39efbd1f2f94
28kayak <kaya_ota@msn.com>
2013-05-06 16:01:47 -0700

    add method named insert()

--- TreeNode.java
+++ TreeNode.java
@@ -11,4 +11,17 @@ public class TreeNode
 		this.left = left;
 		this.right = right;
 	}
+	public int getValue()
+	{
+		return value;
+	}
+	public TreeNode getLeft()
+	{
+		return left;
+	}
+	public TreeNode getRight()
+	{
+		return right;
+	}
+	
 }
