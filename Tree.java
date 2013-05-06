91a3e07a31a9a675b8044ad86f7557a8aadb7485
28kayak <kaya_ota@msn.com>
2013-05-06 15:14:18 -0700

    add searching method contains()

--- Tree.java
+++ Tree.java
@@ -1,7 +1,14 @@
 
-public class Tree
+public class Tree	
 {
-	protected TreeNode root = null;
+	protected TreeNode root;
+	
+	public Tree(int element)
+	{
+		root.left = null;
+		root.right = null;
+		root.value = element;
+	}
 	
 	protected void replace(TreeNode parent, TreeNode newNode, TreeNode oldNode)
 	{
@@ -25,5 +32,39 @@ public class Tree
 			//and parent does not have any child.
 		}
 	}
+	public boolean contains(int target)
+	{
+		return  contains(root, target);
+	}
+	public boolean contains(TreeNode node, int targetElement)
+	{
+		if(node == null)
+		{
+			return false;
+		}
+		if(node.value == targetElement)
+		{
+			return true;
+		}
+		if(targetElement < node.value)
+		{
+			return contains(node.left, targetElement);
+		}
+		else
+		{
+			return contains(node.right, targetElement);
+		}
+	}
+	
+	/*
+	public String toString()
+	{
+		TreeNode cursor;
+		if(this.root != null)
+		{
+			cursor = root.left();
+		}
+	}
+	*/
 
 }
